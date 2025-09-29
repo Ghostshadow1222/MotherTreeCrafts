010f02ed0047d85653a4bdff345fa9758324ce6b
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-09-29 14:06:43 -0700

    Add project files.

---
+++ MotherTreeCrafts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MotherTreeCrafts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
