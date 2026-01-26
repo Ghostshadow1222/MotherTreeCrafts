3ada80ac38fe4dabc52796bfdf9cf92e3bf8f50a
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2026-01-26 07:47:36 -0800

    Add method stubs to InventoryAnalysis class with similar modifications to ProductAnaylsis.
    Added seven new method stubs: Track, Update, Reorder, Reserve, Check, Adjust, and Replenish. Each method currently throws NotImplementedException and includes a TODO comment for future implementation.

--- MotherTreeCrafts/Docs/SkeletonCode/InventoryAnalysis.cs
+++ MotherTreeCrafts/Docs/SkeletonCode/InventoryAnalysis.cs
@@ -2,5 +2,46 @@
 {
     public class InventoryAnalysis
     {
+        // TODO: Implement Track functionality
+        public void Track()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Update functionality
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Reorder functionality
+        public void Reorder()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Reserve functionality
+        public void Reserve()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Check functionality
+        public void Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Adjust functionality
+        public void Adjust()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Replenish functionality
+        public void Replenish()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
