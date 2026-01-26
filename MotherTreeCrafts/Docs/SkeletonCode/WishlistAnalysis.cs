d5c492d9b843052ee8087fafa73b908456cb7f05
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2026-01-26 07:49:53 -0800

    Add method stubs for wishlist management actions with similar changes as ProductAnalysis.
    Added Add, Remove, View, Prioritize, Update, and Manage methods to WishlistAnalysis class. Each method currently throws NotImplementedException and includes a TODO comment for future implementation.

--- MotherTreeCrafts/Docs/SkeletonCode/WishlistAnalysis.cs
+++ MotherTreeCrafts/Docs/SkeletonCode/WishlistAnalysis.cs
@@ -2,5 +2,40 @@
 {
     public class WishlistAnalysis
     {
+        // TODO: Implement Add functionality
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Remove functionality
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement View functionality
+        public void View()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Prioritize functionality
+        public void Prioritize()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Update functionality
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Implement Manage functionality
+        public void Manage()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
